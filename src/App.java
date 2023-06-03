9455289e31c48026af41293885df618fa6e121e2
elizabethtwo <elizabethmachado522@gmail.com>
2023-06-03 17:15:12 -0500

    aaaa

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         for (int i = 0; i < edades.length; i++) {
             System.out.println("edades[" + i + "] = " + edades[i]);
             System.out.println("mamaguevo");
-            //Na mentira
+            //Na mentira XD
         }
     }
 }
