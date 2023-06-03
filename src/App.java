20342d1dd6c3c84004b5b3e7137025e04ff6444d
SaberCris24 <crisdax2442@gmail.com>
2023-06-03 16:42:09 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] edades = new int[] {10, 30, 50, 70, 80, 85};
+        System.out.println("edades = " + Arrays.toString(edades));
+        System.out.println("Elementos = " + edades.length);
+        
+        for (int i = 0; i < edades.length; i++) {
+            System.out.println("edades[" + i + "] = " + edades[i]);
+        }
+    }
+}
