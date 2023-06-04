23f3a750a505726a0f7e38a5cb81ba489e004d1e
SaberCris24 <crisdax2442@gmail.com>
2023-06-04 01:59:37 -0500

    XD

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -4,27 +4,29 @@ import java.util.Arrays;
 import java.io.IOException;
 
 public class Operaciones {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int x = 0;
-
         try {
-            System.out.println("Ingresa el numero de filas");
-            x = Integer.parseInt(br.readLine());
-            int suma [][] = new int[x][1];
+            System.out.println("Ingresa la cantidad de numeros: ");
+            int cnumeros = Integer.parseInt(br.readLine());
 
+            int[] svector = new int[cnumeros];
 
-            
-            //for (int i = 0 ; i < suma.length; i++) {
-              //  int number = 0, number2 = 0;
-              //  number = suma[i][1];
-              //  number2 = suma[i+1][1];
+            System.out.println("Ingresa los numeros: ");
 
-            //}
+            for (int i = 0; i < cnumeros; i++) {
+                System.out.println("Ingresa el numero " + (i+1) + ":");
+                svector[i] = Integer.parseInt(br.readLine());
+            }
+
+            int suma = Arrays.stream(svector).reduce(1, (a, b) -> a / b);
+
+            System.out.println("El resultado es: " + suma);
 
         } catch (NumberFormatException | NegativeArraySizeException e) {
             System.out.println("Tipo de dato incorrecto");
+            br.close();
         }
 
     }
